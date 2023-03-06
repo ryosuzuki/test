c924ee87cf1590f5b5ac5b56bf9bd4d7a457457b
shivesh jadon <shiveshssj@gmail.com>
2023-03-06 01:53:03 -0700

    added all chatgpt prompts

--- chatgpt-test.js
+++ chatgpt-test.js
@@ -30,10 +30,19 @@ const text = rawtext.replace(/(\r\n|\n|\r)/gm, " ")
 console.log(text)
 
 // summary
-// "Provide the summary for the followint text: " 
+// "Provide the summary for the following text: " 
 
 // highlight
-// "Extract important words from the followint text with json array: " 
+// "Extract important words from the following text with json array: " 
+
+// hierarchy
+// ` create a heirarchy of main points for this text in json format. use suitable variable names for headings. use short phrases instead of sentences. your output should be only code json format. don't print any explanation or text: `
+
+// images
+// `give me top 5 words or phrases from this text in a json file. your output should be only code json format. don't print any explanation or text. the json should contain all the results under the variable "keywords": `
+
+// flashcard
+// `can you create flashcards for this given text? give output in json and json only without any additional text. json format should be: title of flashcard and its short description as value. don't use sentences; use short phrases everywhere: `
 
 let prompt = `can you create flashcards for this given text? give output in json and json only without any additional text. json format should be: title of flashcard and its short description as value. don't use sentences; use short phrases everywhere: ` 
 
